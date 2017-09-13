198d879f9e911dc9b1e9a34bf7e70c342aaffbb3
yokin <yokinzhang@gmail.com>
2017-09-13 20:40:14 +0800

    add navbar router-link

--- my_project/frontend/src/components/Navbar.vue
+++ my_project/frontend/src/components/Navbar.vue
@@ -8,9 +8,15 @@
         <el-menu-item index="3"  style="float: right;" @click="handleLogOut()">注销</el-menu-item>
         <el-submenu index="2" style="float: right;">
           <template slot="title">{{ $store.state.user_name }}</template>
-          <el-menu-item index="2-1" v-if="$store.state.user_type == 'designer'">新增订单</el-menu-item>
-          <el-menu-item index="2-2" v-if="$store.state.user_type == 'contractor'">参与竞标</el-menu-item>
-          <el-menu-item index="2-3">账户明细</el-menu-item>
+          <router-link :to="'/newOrder'" class="text">
+            <el-menu-item index="2-1" v-if="$store.state.user_type == 'designer'">新增订单</el-menu-item>
+          </router-link>
+          <router-link :to="'/blueprintList'" class="text">
+            <el-menu-item index="2-2" v-if="$store.state.user_type == 'contractor'">参与竞标</el-menu-item>
+          </route-link>
+          <router-link :to="'/boundList'" class="text">
+            <el-menu-item index="2-3"><route-link>账户明细</el-menu-item>
+          </router-link>
         </el-submenu>
       </el-col>
     </el-row>
@@ -27,7 +33,10 @@ export default{
 }
 </script>
 <style scoped>
-  div{
+  .text {
+    text-decoration: none;
+  }
+  div {
     width: 100%;
   }
 </style>
